cebe9939e8afa3e9671b5a0d655151a692e92e3c
Max Timkovich <mtimkovich@gmail.com>
2018-08-25 05:48:55 +0000

    started work on better tests

---
+++ tests/test_garpr_seeds_challonge.py
@@ -0,0 +1,9 @@
+import json
+from os.path import dirname, abspath
+import sys
+from unittest.mock import Mock
+
+# Add the parent directory to the path
+sys.path.append(dirname(dirname(abspath(__file__))))
+
+import garpr_seeds_challonge
